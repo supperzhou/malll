cea9007a45b3f11804ef77208dafbc4fe154e122
supperzhou <2170597858@qq.com>
2021-10-15 19:15:31 +0800

    user用户mapper

--- src/main/java/com/s3/mapper/UserMapper.java
+++ src/main/java/com/s3/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.s3.mapper;
 
+import com.s3.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface UserMapper {
+    List<User> getUserlogin(@Param("userName") String userName,@Param("userPassword") String userPassword);
+
 }
