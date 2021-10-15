30d13009b106b06c234eaa0ad778e1c69f91f590
jjy <1481233479@qq.com>
2021-10-15 16:24:56 +0800

    Initial commit

---
+++ src/test/java/com/s3/MallApplicationTests.java
@@ -0,0 +1,14 @@
+package com.s3;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
